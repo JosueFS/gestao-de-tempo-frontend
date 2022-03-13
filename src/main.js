caa18c491f9214451060b54f2520b52902ccb463
Ribeirorodrigo05 <Ribeiro.rodrigo05@gmail.com>
2022-03-13 11:59:06 -0300

    origin/feature/Start-frontEnd
    Ínicio do MELHOR PROJETO DO MUNDO, PRA CIMA !!!!!

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/router'
+import { createProvider } from './vue-apollo'
+import vuetify from './plugins/vuetify'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  apolloProvider: createProvider(),
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
